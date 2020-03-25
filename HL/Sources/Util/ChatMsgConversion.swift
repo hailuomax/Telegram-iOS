bb7e08fbaf018b12d2dd94173f3c8d7b786fc613
JY <604362118@qq.com>
2020-03-25 10:21:05 +0800

    增加ChatMsgConversion 聊天内容转换工具

--- HL/Sources/Util/ChatMsgConversion.swift
+++ HL/Sources/Util/ChatMsgConversion.swift
@@ -8,6 +8,78 @@
 import Foundation
 import CryptoSwift
 
+/// 聊天内容转换工具，用于转换显示红包气泡等
+public struct ChatMsgConversion {
+    
+}
+
+extension ChatMsgConversion: InputOutputStype{
+    
+    typealias Input = String
+    
+    typealias Output = ChatMsgEnum
+    
+    public func transform(input: String) -> ChatMsgEnum {
+        return ChatMsgEnum.unknow(type: "0")
+    }
+}
+
+extension ChatMsgConversion {
+ 
+    
+    ///会话列表解析V1的红包
+    private func decodeV1(_ messageStr: String) -> ChatMsgEnum? {
+        
+        let infos = messageStr.components(separatedBy: ChatMsgConfig.V1.Heart)
+        
+        //【♥下载海螺 APP，体验红包新功能♥恭喜發財，大吉大利♥MS0xLTEyMzI1OTczMzMxODEwNjMxNzAtODc3MDM1ODI3LTg3NzAzNTgyNw==♥A930037540F7FDB11C3367601540D868♥】
+        guard infos.count == 6 else {return nil}
+        
+        //base64与md5校验
+        let strFromBase64: String = infos[3].jy.base64Decoding() ?? ""
+        let remark = infos[2]
+        
+        let finalMd5Str = md5TransactionInfos(strFromBase64, remark: remark)
+        guard finalMd5Str == infos[4] else {return nil}
+        
+        print(infos)
+        
+        var transactionInfos = strFromBase64.components(separatedBy: "-")
+        if transactionInfos[0] != "\(ChatMsgConfig.V1.version)" { //版本号不对,后续数据都为空
+            transactionInfos = transactionInfos.enumerated().compactMap({
+                return ($0 == 0 || $0 == 1) ? $1 : ""
+            })
+        }
+        print(transactionInfos)
+        
+        switch transactionInfos[1] {
+        case "1":
+            guard transactionInfos.count >= 4 else {return nil}
+            return .redPacket(version: ChatMsgConfig.V1.version, type: "1", id: transactionInfos[1], senderId: transactionInfos[2], recipientId: transactionInfos[3], remark: remark)
+        case "2":
+            guard transactionInfos.count >= 4 else {return nil}
+            return .transfer(version: ChatMsgConfig.V1.version, type: "2", id: transactionInfos[1], senderId: transactionInfos[2], recipientId: transactionInfos[3], remark: remark)
+        case "3":
+            guard transactionInfos.count >= 6 else {return nil}
+            return .exchange(version: ChatMsgConfig.V1.version, type: "3", id: transactionInfos[1], senderId: transactionInfos[2], recipientId: transactionInfos[3], payCoin: transactionInfos[4], getCoin: transactionInfos[5])
+        default:
+            return nil
+        }
+        
+    }
+}
+
+///md5交易信息
+fileprivate func md5TransactionInfos(_ str: String, remark: String) -> String{
+    let md5Str = str + remark
+    let messageBase64Key: String = APPConfig.environment.getSignKeyAndIv().kMessageBase64Key
+    return (md5Str.md5().uppercased() + (messageBase64Key.jy.base64Decoding())).md5().uppercased()
+}
+
+public enum MyError: Error {
+    case modelError
+}
+
 /// 聊天交易相关的气泡类型
 public enum ChatMsgEnum {
     
@@ -88,76 +160,4 @@ public enum ChatMsgEnum {
             return ""
         }
     }
-}
-
-/// 聊天内容转换工具，用于转换显示红包气泡等
-public struct ChatMsgConversion {
-    
-}
-
-extension ChatMsgConversion: InputOutputStype{
-    
-    typealias Input = String
-    
-    typealias Output = ChatMsgEnum
-    
-    public func transform(input: String) -> ChatMsgEnum {
-        return ChatMsgEnum.unknow(type: "0")
-    }
-}
-
-extension ChatMsgConversion {
- 
-    
-    ///会话列表解析V1的红包
-    private func decodeV1(_ messageStr: String) -> ChatMsgEnum? {
-        
-        let infos = messageStr.components(separatedBy: ChatMsgConfig.V1.Heart)
-        
-        //【♥下载海螺 APP，体验红包新功能♥恭喜發財，大吉大利♥MS0xLTEyMzI1OTczMzMxODEwNjMxNzAtODc3MDM1ODI3LTg3NzAzNTgyNw==♥A930037540F7FDB11C3367601540D868♥】
-        guard infos.count == 6 else {return nil}
-        
-        //base64与md5校验
-        let strFromBase64: String = infos[3].jy.base64Decoding() ?? ""
-        let remark = infos[2]
-        
-        let finalMd5Str = md5TransactionInfos(strFromBase64, remark: remark)
-        guard finalMd5Str == infos[4] else {return nil}
-        
-        print(infos)
-        
-        var transactionInfos = strFromBase64.components(separatedBy: "-")
-        if transactionInfos[0] != "\(ChatMsgConfig.V1.version)" { //版本号不对,后续数据都为空
-            transactionInfos = transactionInfos.enumerated().compactMap({
-                return ($0 == 0 || $0 == 1) ? $1 : ""
-            })
-        }
-        print(transactionInfos)
-        
-        switch transactionInfos[1] {
-        case "1":
-            guard transactionInfos.count >= 4 else {return nil}
-            return .redPacket(version: ChatMsgConfig.V1.version, type: "1", id: transactionInfos[1], senderId: transactionInfos[2], recipientId: transactionInfos[3], remark: remark)
-        case "2":
-            guard transactionInfos.count >= 4 else {return nil}
-            return .transfer(version: ChatMsgConfig.V1.version, type: "2", id: transactionInfos[1], senderId: transactionInfos[2], recipientId: transactionInfos[3], remark: remark)
-        case "3":
-            guard transactionInfos.count >= 6 else {return nil}
-            return .exchange(version: ChatMsgConfig.V1.version, type: "3", id: transactionInfos[1], senderId: transactionInfos[2], recipientId: transactionInfos[3], payCoin: transactionInfos[4], getCoin: transactionInfos[5])
-        default:
-            return nil
-        }
-        
-    }
-}
-
-///md5交易信息
-fileprivate func md5TransactionInfos(_ str: String, remark: String) -> String{
-    let md5Str = str + remark
-    let messageBase64Key: String = APPConfig.environment.getSignKeyAndIv().kMessageBase64Key
-    return (md5Str.md5().uppercased() + (messageBase64Key.jy.base64Decoding())).md5().uppercased()
-}
-
-public enum MyError: Error {
-    case modelError
 }
