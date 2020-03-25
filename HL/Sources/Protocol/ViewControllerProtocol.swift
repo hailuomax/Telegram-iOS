e911cf2d227461be460864583ac30aa3e032bc4e
JY <604362118@qq.com>
2020-03-25 16:08:20 +0800

    去除HL相关工程

--- HL/Sources/Protocol/ViewControllerProtocol.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ViewControllerProtocol.swift 与控制器相关的协议都放这里
-//  HL
-//
-//  Created by 黄国坚 on 2020/3/23.
-//
-
-import Foundation
--- HL/Sources/Protocol/ViewProtocol.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ViewProtocol.swift 与view相关的协议都放这里
-//  HL
-//
-//  Created by 黄国坚 on 2020/3/23.
-//
-
-import Foundation
