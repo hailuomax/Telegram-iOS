4e0ed472207b9b73f66cad43318f1ba8525d0416
JY <604362118@qq.com>
2020-04-01 19:45:59 +0800

    设置清理项目内xcodeproj文件的命令

--- clean.sh
+++ clean.sh
@@ -1,6 +1,2 @@
-rm -rf App/App.xcodeproj App/App.xcworkspace
-rm -rf submodules/MtProtoKit/MtProtoKit.xcodeproj
-rm -rf submodules/Emoji/Emoji.xcodeproj
-rm -rf Project.xcodeproj
-rm -rf Telegram_Buck.xcworkspace
-rm -rf buck-out
+find . -name "*.xcodeproj" | xargs  rm -rf
+find . -name "*.xcworkspace" | xargs  rm -rf
