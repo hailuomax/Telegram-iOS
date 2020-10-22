c366c176a6aee5b0d28b2c7839f608020c3e6730
fan <365189523@qq.com>
2020-10-22 17:32:04 +0800

    修复联系人页滚动到顶部有间隙

--- submodules/ContactListUI/Sources/ContactListNode.swift
+++ submodules/ContactListUI/Sources/ContactListNode.swift
@@ -1504,6 +1504,6 @@ public final class ContactListNode: ASDisplayNode {
     }
     
     @objc public func scrollToTop() {
-        self.listNode.transaction(deleteIndices: [], insertIndicesAndItems: [], updateIndicesAndItems: [], options: [.Synchronous, .LowLatency], scrollToItem: ListViewScrollToItem(index: 0, position: .top(-50.0), animated: true, curve: .Default(duration: nil), directionHint: .Up), updateSizeAndInsets: nil, stationaryItemRange: nil, updateOpaqueState: nil, completion: { _ in })
+        self.listNode.transaction(deleteIndices: [], insertIndicesAndItems: [], updateIndicesAndItems: [], options: [.Synchronous, .LowLatency], scrollToItem: ListViewScrollToItem(index: 0, position: .top(0), animated: true, curve: .Default(duration: nil), directionHint: .Up), updateSizeAndInsets: nil, stationaryItemRange: nil, updateOpaqueState: nil, completion: { _ in })
     }
 }
